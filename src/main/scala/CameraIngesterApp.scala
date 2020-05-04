695b667c4f8bb5a212370c8cb0cdadd1abc8a2bd
Christian Rodemeyer <atombrenner@gmail.com>
2020-05-04 14:53:15 +0200

    import from Dropbox

--- src/main/scala/CameraIngesterApp.scala
+++ src/main/scala/CameraIngesterApp.scala
@@ -5,24 +5,24 @@ object CameraIngesterApp extends App {
 
   println("Camera Ingester")
 
-  // sudo mkdir /run/media/data
-  // sudo mount /dev/sdb2 /run/media/data
+  // sudo mkdir ~/Data
+  // sudo mount /dev/sda2 ~/Data
 
-  val photoLibrary = new MediaLibrary(Paths.get("/run/media/data/Daten/Bilder/Photos/"), MediaFile.jpg)
-  val videoLibrary = new MediaLibrary(Paths.get("/run/media/data/Daten/Video/Eigene/"), MediaFile.mp4)
+  val photoLibrary = new MediaLibrary(Paths.get("/home/christian/Data/Daten/Bilder/Photos/"), MediaFile.jpg)
+  val videoLibrary = new MediaLibrary(Paths.get("/home/christian/Data/Daten/Video/Eigene/"), MediaFile.mp4)
 
   // Parameters
   // MediaFolder
   // Optional Source
 
-  val roots = FileSystems.getDefault.getRootDirectories.asScala
-  val potentialSources = if (roots.head.toString == "/") { // Linux
-    Files.list(roots.head.resolve("run/media").resolve(System.getProperty("user.name"))).iterator.asScala
-  } else {
-    roots
-  }
-  var sources = potentialSources.map(_.resolve("DCIM")).filter(Files.isDirectory(_)).toSeq
-  //sources = Seq(Paths.get("/data/Google Drive/Google Fotos/")) ++ sources
+//  val roots = FileSystems.getDefault.getRootDirectories.asScala
+//  val potentialSources = if (roots.head.toString == "/") { // Linux
+//    Files.list(roots.head.resolve("run/media").resolve(System.getProperty("user.name"))).iterator.asScala
+//  } else {
+//    roots
+//  }
+//  var sources = potentialSources.map(_.resolve("DCIM")).filter(Files.isDirectory(_)).toSeq
+  var sources = Seq(Paths.get("/home/christian/Dropbox/DCIM")) // ++ sources
 
   sources.foreach{ source =>
     println(s"Importing from $source")
@@ -33,7 +33,7 @@ object CameraIngesterApp extends App {
     // TODO: delete empty folders inside of DCIM folders
 
     if (Files.list(source).count == 0) {
-      Files.delete(source)
+      // Files.delete(source)
     } else {
       println(s"Warning: $source not empty")
     }
